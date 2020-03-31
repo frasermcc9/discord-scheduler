877653e4490cb31461ffde009fa928b4f28c8475
Fraser McCallum <fraser@themccallums.net.nz>
2020-03-31 17:14:23 +1300

    Added confirmation to message

--- commands/core/schedule.js
+++ commands/core/schedule.js
@@ -32,23 +32,42 @@ module.exports = class ScheduleCommand extends Command {
         d = d.map(Number)
         let date = new Date(d[0], d[1] - 1, d[2], d[3], d[4], d[5])
 
+
+        let reactionArray = ['✅', '❌']
+        const filter = (reaction, user) => {
+            return reactionArray.includes(reaction.emoji.name) && user.id == msg.author.id;
+        };
+
         let confirmMessage = new Discord.MessageEmbed()
             .setTitle("New Scheduled Direct Message")
             .setDescription(`Sending at: ${date}`)
             .addField(bold('Content'), `${dmContent}`)
             .addField(bold('Recipient'), `${role}`)
             .setColor('#37db00')
-        msg.say(confirmMessage)
+        msg.say(confirmMessage).then(msg => msg.react('✅')).then(async m => {
+            await m.message.react('❌')
 
-        let j = schedule.scheduleJob(date, function () {
-            let membersWithRole = msg.guild.roles.find(r => r == role).members.map(m => m.user);
-            membersWithRole.forEach(el => {
-                el.send(dmContent).catch(() => { console.log(`Tried to send a dm to ${el.username}, but couldn't. Likely due to privacy settings or the user was a bot.`) })
+            const collector = new Discord.ReactionCollector(m.message, filter, { time: 1000 * 45 })
+            collector.on('collect', () => {
+                collector.stop()
             })
-        });
+            collector.on('end', collected => {
+                msg.say('Confirmed Message')
+                let reactData = collected.first()
+                if (typeof (reactData) == 'undefined' || reactData.emoji.name == '❌')
+                    return msg.say(`You cancelled the message`)
+                else if (reactData.emoji.name == '✅') {
+                    let j = schedule.scheduleJob(date, function () {
+                        let membersWithRole = msg.guild.roles.cache.find(r => r == role).members.map(m => m.user);
+                        membersWithRole.forEach(el => {
+                            el.send(dmContent).catch(() => { console.log(`Tried to send a dm to ${el.username}, but couldn't. Likely due to privacy settings or the user was a bot.`) })
+                        })
+                    });
+
+                }
+            })
+        })
     }
 }
 
-const bold = (txt) => {
-    return '**' + txt + '**'
-}
+const bold = (txt) => '**' + txt + '**'
